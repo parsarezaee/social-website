89cc8100ff0771eba94820ffbd701cdff6f18d59
parsarezaee <parsarezaee777@yahoo.com>
2022-01-17 23:46:51 +0330

    first commit

---
+++ account/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserRegisterForm, UserLoginForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
+
+class RegisterView(View):
+    form_class = UserRegisterForm
+    template_name = 'account/register.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'You registered successfuly', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form':form})
