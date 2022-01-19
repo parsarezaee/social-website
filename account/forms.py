45fd435707157a7681f30e4e667650f318017355
parsarezaee <parsarezaee777@yahoo.com>
2022-01-19 23:01:23 +0330

    override clean for register form

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,11 @@
+from logging.config import valid_ident
 from django import forms
 from django import forms
 from django.forms.widgets import PasswordInput 
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
 
 class UserLoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
@@ -10,3 +15,10 @@ class UserRegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'id':'pass'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('This email already exists')
+        return email
