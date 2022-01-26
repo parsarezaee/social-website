e26bc68130322e5aef7dfd19c17c4f5aaa76924b
parsarezaee <parsarezaee777@yahoo.com>
2022-01-26 07:40:18 +0330

    edit admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import PostUser
 
-admin.site.register(PostUser)
+
+@admin.register(PostUser)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slug', 'created')
+    list_filter = ('updated',)
+    search_fields = ['slug']
+    prepopulated_fields = {'slug':('text',)}
+    raw_id_fields = ('user',)
